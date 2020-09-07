f85ed7a47a0945a808e46228b2ab1a09e00d4b67
Mickey Chen <poverwhelming@gmail.com>
2020-09-07 14:57:21 +0800

    formatted

--- lib/tracker_web/controllers/interest_controller.ex
+++ lib/tracker_web/controllers/interest_controller.ex
@@ -2,6 +2,6 @@ defmodule TrackerWeb.InterestController do
   use TrackerWeb, :controller
 
   def index(conn, _) do
-    render(conn, interests: (1..10))
+    render(conn, interests: 1..10)
   end
 end
